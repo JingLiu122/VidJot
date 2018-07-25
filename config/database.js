5090557f76bb550a0f6d7eef42b5666b787a30b3
Jing L <jingl@Jings-MacBook-Pro.local>
2018-07-25 17:49:01 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV === 'production'){
+	module.exports = {mongoURI: 'mongodb://Jing:icecream123@ds155461.mlab.com:55461/vidjot-prod'}
+}else{
+	module.exports = {mongoURI: 'mongodb://localhost:27017/vidjot-dev'}
+}
